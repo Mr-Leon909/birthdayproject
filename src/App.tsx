57d3a5f3e72e696c83f29307e05a0a6645e74981
T-Y89 <0809taikiyamashita@gmail.com>
2025-05-16 23:46:45 +0900

    opening

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import BirthdayIntro from './BirthdayIntro';
 import TopPage from './components/TopPage';
 import SNSPage from './components/SNSPage';
@@ -8,36 +8,48 @@ import Navigation from './components/Navigation';
 import SecretPage from './components/secretPage';
 import logoImage from './assets/logo.png';
 
-
-function App() {
-  const [currentPage, setCurrentPage] = useState(() => {
-    return localStorage.getItem('introSeen') ? 'top' : 'intro';
-  });
+// App内のロジックとLocationObserverを分離
+const AppContent = () => {
+  const [currentPage, setCurrentPage] = useState('intro');
+  const [showIntroOnHome, setShowIntroOnHome] = useState(true);
+  const location = useLocation();
 
   const handleBirthdayIntroFinish = () => {
-    // 初回閲覧済フラグを保存
-    localStorage.setItem('introSeen', 'true');
     setCurrentPage('top');
+    setShowIntroOnHome(false);
   };
+  
+  // locationの変更を監視
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setShowIntroOnHome(true);
+    }
+  }, [location]);
 
   return (
+    <div>
+      {(currentPage === 'intro' || (showIntroOnHome && location.pathname === '/')) ? (
+        <BirthdayIntro onFinish={handleBirthdayIntroFinish} />
+      ) : (
+        <div className="z-10">
+          <img src={logoImage} alt="Logo" className="mt-3 left-7 w-12 h-13" />
+          <Navigation/>
+          <Routes>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/sns/*" element={<SNSPage />} />
+            <Route path="/quiz/*" element={<Quiz />} />
+            <Route path="/secret" element={<SecretPage />} />
+          </Routes>
+        </div>
+      )}
+    </div>
+  );
+};
+
+function App() {
+  return (
     <Router>
-      <div>
-        {currentPage === 'intro' ? (
-          <BirthdayIntro onFinish={handleBirthdayIntroFinish} />
-        ) : (
-            <div className="z-10">
-            <img src={logoImage} alt="Logo" className="mt-3 left-7 w-12 h-13" />
-            <Navigation/>
-            <Routes>
-              <Route path="/" element={<TopPage />} />
-              <Route path="/sns/*" element={<SNSPage />} />
-              <Route path="/quiz/*" element={<Quiz />} />
-              <Route path="/secret" element={<SecretPage />} />
-            </Routes>
-          </div>
-        )}
-      </div>
+      <AppContent />
     </Router>
   );
 }
